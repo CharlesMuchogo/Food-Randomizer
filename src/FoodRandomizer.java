ebf0a325e5479499e4fcb25a945e4f7329e6945c
CharlesMuchogo <muchpaul2@gmail.com>
2022-08-07 05:18:35 +0300

    initial commit

--- src/FoodRandomizer.java
+++ src/FoodRandomizer.java
@@ -1,2 +1,82 @@
-package PACKAGE_NAME;public class FoodRandomizer {
+import java.sql.*;
+import java.util.ArrayList;
+
+public class  FoodRandomizer {
+    final String databaseUrl = "jdbc:mysql://localhost:3306/randomizer";
+    final String username = "root";
+    final  String password = "";
+
+    ArrayList<String> maindish = new ArrayList<>();
+    ArrayList<String> stew = new ArrayList<>() ;
+
+    public  void retrieveData(String type){
+        try {
+            Connection connection = DriverManager.getConnection(databaseUrl, username,password);
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM food  WHERE type = ?");
+            statement.setString(1, type);
+
+            ResultSet rs = statement.executeQuery();
+            rs.next();
+
+        while (rs.next()){
+            if(type == "Main Dish"){
+                maindish.add(rs.getString("food"));
+
+            }else {
+                stew.add(rs.getString("food"));
+
+            }
+        }
+
+            rs.close();
+        } catch (SQLException e){
+            System.out.println("Caught error: " + e.toString());
+        }
+    }
+
+    public void getFoodChoice(){
+        String randomStew = "";
+        String randomDish = "";
+
+        retrieveData("Main Dish");
+        retrieveData("Stew");
+        for (int i = 0; i < maindish.size(); i++)
+        {
+
+            int index = (int)(Math.random() * maindish.size());
+
+        randomDish = maindish.get(index);
+        }
+        for (int i = 0; i < stew.size(); i++)
+        {
+
+            int index = (int)(Math.random() * stew.size());
+
+            randomStew = stew.get(index);
+        }
+
+        maindish.clear();
+        stew.clear();
+
+        System.out.println(" Today you will be having " + randomDish + " and " + randomStew + " \n");
+    }
+
+    public void addData(String name, String type){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(databaseUrl, username,password);
+
+            PreparedStatement statement = connection.prepareStatement("Insert Into  food(food, type )values(?,?)");
+            statement.setString(1, name);
+            statement.setString(2, type);
+
+            statement.executeUpdate();
+            System.out.println("You have added " + name + " to " + type);
+
+        } catch (SQLException e){
+            System.out.println("Caught error: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.sql.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+
+        FoodRandomizer database = new FoodRandomizer();
+        System.out.println(">>>>>>>>>>>>>>> Welcome To Food Randomizer <<<<<<<<<<<<<<<");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Press A to add food, and V to view today's meal, and Q to exit: \n");
+        String choice = scan.next().trim().toUpperCase();
+
+
+        while (!choice.equals("Q") ){
+
+            if(choice.equals("A")){
+                System.out.println("Enter the type of food you wish to add: S for stew and M for main food \n");
+
+
+                String typeOfFood = scan.next().trim().toUpperCase();
+
+                if (typeOfFood.equals("S")){
+                    System.out.println("Enter the stew name: \n");
+                    String food = scan.next();
+
+
+                    database.addData(food, "Stew");
+
+                } else if (typeOfFood.equals("M")) {
+                    System.out.println("Enter the main dish name: \n");
+                    String food = scan.next();
+                    database.addData(food, "Main Dish");
+
+
+                }
+
+
+
+            } else if (choice.equals("V")) {
+//
+
+                database.getFoodChoice();
+            }
+            System.out.println("Press A to add food, and V to view today's meal, and Q to exit: \n");
+           choice = scan.next().trim().toUpperCase();
+        }
+
+        }
+
+
 }
